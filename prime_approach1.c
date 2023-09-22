602f34a7a586316640b3e9c6ca53eb3a207af742
Lokeek Lokhande <lokeeklokhande1185@gmail.com>
2023-09-22 23:03:19 +0530

    Update prime_approach1.c

--- prime_approach1.c
+++ prime_approach1.c
@@ -7,26 +7,16 @@ int flag = 1;
 printf("Enter the number you to check for prime number\n");
 scanf("%d",&num);
 
-
-
 for(int i=2; i<num-1;i++){
-
-
-  if(k%i==0){
-  flag =1;
-  break;     
+    if(k%i==0){
+    flag =1;
+    break;     
   }
-  
   if(flag==1){
   printf("The number is not prime");
   }
   else {
   printf("The number here is prime number");
-
 }
-
-
-
-
 }
 
