d817f30591ea4a8b80360fc7aa434c9fdf778321
Lokeek Lokhande <lokeeklokhande1185@gmail.com>
2023-09-23 16:48:24 +0545

    Update matrix1.c

--- matrix1.c
+++ matrix1.c
@@ -51,23 +51,27 @@ int main(){
         		}
         		printf("\n");
      	            }
-/* Digonal Addtion
+// Digonal Addtion
+		
 		int digonalAddition = 0;
-		for (int i = 0; i<3;i++){
-			for (int j = 0; j<3;j++){
-				result[i][j] = matrix1[i][j] + matrix2[i][j]; 
-				if(i==j){
-					printf("Digonal addition");
-					}
-					else{
-   						result[i][j] = 0;
-		for(int i = 0;i<3;i++){
-        		for(int j = 0;j<3;j++){
-        			printf("%d ",result[i][j]);
-        		}
-        		printf("\n");
-     	            }    	     */       
-     	break;
+			for (int i = 0; i<rows; i++){
+ 				 for (int j = 0; j<cols; j++){
+    					result[i][j] = matrix1[i][j] + matrix2[i][j];
+   						 if(i==j){
+   						   digonalAddition += result[i][j];
+   				 }
+ 			 }
+		}
+		
+		int secondDigonalAddition = 0;
+		for (int i = 0; i < rows; i++) {
+ 			 for (int j = cols - 1 - i; j >= 0; j--) {
+  				  secondDigonalAddition += result[i][j];
+  			}
+		}
+printf("Second diagonal addition: %d\n", secondDigonalAddition);
+printf("Diagonal addition: %d\n", digonalAddition);
+   
      	case 2: 
      		for(int i = 0;i<rows;i++){
         		for(int j = 0;j<cols;j++){
