2828a6ad2f53e6d3b008cb26aef56d2b195db90d
Lokeek Lokhande <lokeeklokhande1185@gmail.com>
2023-09-23 15:21:34 +0545

    Add files via upload

--- matrix1.c
+++ matrix1.c
@@ -1,81 +1,102 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<math.h>
 
-int main() {
-    int rows, cols;
-
-    // Get the dimensions of the matrices
-    printf("Enter the number of rows: ");
-    scanf("%d", &rows);
-    printf("Enter the number of columns: ");
-    scanf("%d", &cols);
-
-    // Declare the matrices
-    int matrix1[rows][cols], matrix2[rows][cols], result[rows][cols];
-
-    // Get the elements of the first matrix
-    printf("Enter the elements of the first matrix:\n");
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            scanf("%d", &matrix1[i][j]);
-        }
-    }
-
-    // Get the elements of the second matrix
-    printf("Enter the elements of the second matrix:\n");
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            scanf("%d", &matrix2[i][j]);
-        }
-    }
-
-    // Choose the operation to perform
-    int choice;
-    printf("Choose the operation to perform:\n");
-    printf("1. Addition\n");
-    printf("2. Subtraction\n");
-    printf("3. Multiplication\n");
-    printf("Enter your choice: ");
-    scanf("%d", &choice);
-
-    // Perform the selected operation
-    switch (choice) {
-        case 1: // Addition
-            for (int i = 0; i < rows; i++) {
-                for (int j = 0; j < cols; j++) {
-                    result[i][j] = matrix1[i][j] + matrix2[i][j];
-                }
-            }
-            break;
-        case 2: // Subtraction
-            for (int i = 0; i < rows; i++) {
-                for (int j = 0; j < cols; j++) {
-                    result[i][j] = matrix1[i][j] - matrix2[i][j];
-                }
-            }
-            break;
-        case 3: // Multiplication
-            for (int i = 0; i < rows; i++) {
-                for (int j = 0; j < cols; j++) {
-                    result[i][j] = 0;
-                    for (int k = 0; k < cols; k++) {
-                        result[i][j] += matrix1[i][k] * matrix2[k][j];
-                    }
-                }
-            }
-            break;
-        default:
-            printf("Invalid choice!\n");
-            return 1;
-    }
-
-    // Print the result matrix
-    printf("The result matrix is:\n");
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            printf("%d ", result[i][j]);
-        }
-        printf("\n");
-    }
-
-    return 0;
-}
+int main(){
+	int rows;
+	int cols;
+	printf("Enter the number of rows: ");
+	scanf("%d",&rows);
+	
+	printf("Enter the number of coloumns: ");
+	scanf("%d",&cols);
+	
+	int matrix1[rows][cols];
+	int matrix2[rows][cols];
+	int result[rows][cols];
+	
+	printf("Enter the value of matrix1\n");
+	for(int i=0;i<rows;i++){
+		for(int j=0;j<cols;j++){
+			scanf("%d",&matrix1[i][j]);
+		}
+	}		
+	printf("Enter the value of matrix2\n");
+	for(int i=0;i<rows;i++){
+		for(int j=0;j<cols;j++){
+			scanf("%d",&matrix2[i][j]);
+		}
+	}
+        if(rows!=cols && cols!=rows){
+        printf("Matrix operations not possible");
+        int choice;
+        printf("Enter the operator you want to perform\n");\
+        printf("1.Addition\n");
+        printf("2.Substraction\n");
+        printf("3.Multiplication\n");
+        printf("Enter the choice: ");
+        scanf("%d",&choice);
+        
+        switch (choice){
+        case 1: 
+        	
+           
+        	for(int i = 0;i<rows;i++){
+        		for(int j = 0;j<cols;j++){
+        			result[i][j] = matrix1[i][j] + matrix2[i][j];
+        		}
+     	            }
+     	        for(int i = 0;i<rows;i++){
+        		for(int j = 0;j<cols;j++){
+        			printf("%d ",result[i][j]);
+        		}
+        		printf("\n");
+     	            }
+/* Digonal Addtion
+		int digonalAddition = 0;
+		for (int i = 0; i<3;i++){
+			for (int j = 0; j<3;j++){
+				result[i][j] = matrix1[i][j] + matrix2[i][j]; 
+				if(i==j){
+					printf("Digonal addition");
+					}
+					else{
+   						result[i][j] = 0;
+		for(int i = 0;i<3;i++){
+        		for(int j = 0;j<3;j++){
+        			printf("%d ",result[i][j]);
+        		}
+        		printf("\n");
+     	            }    	     */       
+     	break;
+     	case 2: 
+     		for(int i = 0;i<rows;i++){
+        		for(int j = 0;j<cols;j++){
+        			result[i][j] = matrix1[i][j] - matrix2[i][j];
+        		}
+     	}
+     	 for(int i = 0;i<rows;i++){
+        		for(int j = 0;j<cols;j++){
+        			printf("%d ",result[i][j]);
+        		}
+        		printf("\n");
+     	            }
+        break;
+        case 3: 
+        	for(int i = 0;i<rows;i++){
+        		for(int j = 0;j<cols;j++){
+        			result[i][j] = matrix1[i][j] * matrix2[i][j];
+        		}
+     	}
+        for(int i = 0;i<rows;i++){
+        		for(int j = 0;j<cols;j++){
+        			printf("%d ",result[i][j]);
+        		}
+        		printf("\n");
+     	            }	            
+     	            
+        return 0;
+  }   
+  }
+  }
+     
+        
